d9a1710824375d0c924f6a90b9e65394969b7261
Max Eng <maxpersg@gmail.com>
2015-09-28 21:53:32 +0800

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :mains
   get 'poweroff' => "mains#poweroff"
   get 'poweron' => "mains#poweron"
+  root 'mainss#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
